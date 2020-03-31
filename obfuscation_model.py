ba80bdde723ce9450aac41368ad30720dc8ea6fa
Felix Labelle <flx.labelle@gmail.com>
2020-03-31 16:38:20 -0400

    Working random replament

--- obfuscation_model.py
+++ obfuscation_model.py
@@ -3,6 +3,7 @@ import sklearn
 import argparse
 from nltk import tokenize
 import pickle
+from random import choice
 
 # TODO 1: Get the setup working from end-to-end, without any replacement
 # TODO 2: Random replacement
@@ -21,11 +22,21 @@ import pickle
 # Adapt the other script to this end
 
 # Train, test, and cache the basic classifier for Obfuscation HW
+def load_gendered_words(female_load_path = "female.txt", male_load_path = "male.txt"):
+	female_words = []
+	male_words = []
+	with open(female_load_path) as female_fh:
+		female_words = [word.strip() for word in female_fh.readlines()]
+	with open(male_load_path) as male_fh:
+		male_words = [word.strip() for word in male_fh.readlines()]
+	return set(male_words), set(female_words)
 
-def replacement_func(replacement_type, male_words, female_words):
-	def replacer(text):
-		pass
-	return text
+
+def replacement_selection(word_rep_func):
+	def replacement_function(row):
+		input_tokens = tokenize.word_tokenize(row['post_text'])
+		return " ".join([word_rep_func(token, row['op_gender']) for token in input_tokens])
+	return replacement_function
 
 def main():
 	parser = argparse.ArgumentParser()
@@ -33,14 +44,24 @@ def main():
 	parser.add_argument("--output_file", default="./test.csv")
 	parser.add_argument("--replacement", default="none", choices=['none', 'random', 'semantic'])
 	args = parser.parse_args()
-
 	input_data = pandas.read_csv(args.input_file)
-	input_text = list(input_data["post_text"])
-	input_tokens = [tokenize.word_tokenize(t) for t in input_text]
-	# TODO: Add replacement here
-	replaced_tokens = obfuscate()
-	output_strings = [" ".join(tokens) for tokens in input_tokens]
-	input_data["post_text"] = output_strings
+	gendered_word_lut = {'M': {}, 'W':{}}
+	male_vocab, female_vocab = load_gendered_words()
+	#import pdb;pdb.set_trace()
+	if args.replacement == "random":
+		for word in male_vocab:
+			gendered_word_lut['M'][word] = list(female_vocab)
+		for word in female_vocab:
+			gendered_word_lut['W'][word] = list(male_vocab)
+	elif args.replacement == "semantic":
+		pass
+	else:
+		pass
+	def replacement(token, gender):
+		return choice(gendered_word_lut[gender].get(token, [token]))
+	
+	processed_text = input_data.apply(replacement_selection(replacement), axis = 1) # place
+	input_data["post_text"] = processed_text
 	input_data.to_csv(args.output_file)
 
 if __name__ == "__main__":
